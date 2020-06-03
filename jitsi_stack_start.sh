a5936bc0c6f0d59455f2ff29c3a7470da2cabd78
Osmell Caicedo <correo.oele@gmail.com>
2020-06-03 14:46:47 -0500

    updat public url in .env file

--- jitsi_stack_start.sh
+++ jitsi_stack_start.sh
@@ -1,5 +1,5 @@
 # EDIT the CONFIG, point to the location of your jitsi meet cfg location (ex: /opt/jitsi-meet-cfg)
 #      the PUBLIC_URL: your jitsi public url
 
-export $(cat .env) \
+export $(cat .env)
 docker stack deploy -c jitsi.yml jitsi
